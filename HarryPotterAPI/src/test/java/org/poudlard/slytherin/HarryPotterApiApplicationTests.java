d978369439c50e83aab26a27301df2b9e1f13689
Thomas Blondel <tfoenix@gmail.com>
2023-03-06 15:50:15 +0100

    [Project] Init with rider

---
+++ HarryPotterAPI/src/test/java/org/poudlard/slytherin/HarryPotterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poudlard.slytherin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
