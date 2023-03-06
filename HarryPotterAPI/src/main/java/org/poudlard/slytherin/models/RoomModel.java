34848b7e9ffa46cc799734fa5d14926bca39fd45
Thomas Blondel <tfoenix@gmail.com>
2023-03-06 23:00:17 +0100

    [Model] Created RoomModel

---
+++ HarryPotterAPI/src/main/java/org/poudlard/slytherin/models/RoomModel.java
@@ -0,0 +1,51 @@
+package org.poudlard.slytherin.models;
+
+import java.util.Date;
+
+public class RoomModel {
+    private String name;
+    private RoomType type;
+    private int floorArea;
+    private Date creationDate;
+    private String creatorName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    public int getFloorArea() {
+        return floorArea;
+    }
+
+    public void setFloorArea(int floorArea) {
+        this.floorArea = floorArea;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+}
