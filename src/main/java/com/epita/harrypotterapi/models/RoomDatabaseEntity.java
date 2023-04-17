88495d8b4e80361d301ccf02cb89c1e71131bc10
Thomas Blondel <tfoenix@gmail.com>
2023-04-17 12:24:51 +0200

    [Model] RoomDatabaseEntity updated, added relation and primary key

--- src/main/java/com/epita/harrypotterapi/models/RoomDatabaseEntity.java
+++ src/main/java/com/epita/harrypotterapi/models/RoomDatabaseEntity.java
@@ -1,18 +1,23 @@
 package com.epita.harrypotterapi.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
 public class RoomDatabaseEntity {
-    @Id private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private RoomType Type;
     private int area;
     private Date creationDate;
     private String creatorName;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ReservationDatabaseEntity> reservations;
 }
