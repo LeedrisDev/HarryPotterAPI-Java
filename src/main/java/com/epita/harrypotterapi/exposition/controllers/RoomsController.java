6424a12f9ffeceb0b2604af4c11263358df9dfc1
Thomas Blondel <tfoenix@gmail.com>
2023-05-05 15:45:43 +0200

    [RoomsController] Added param in functions and RoomService attribute

--- src/main/java/com/epita/harrypotterapi/exposition/controllers/RoomsController.java
+++ src/main/java/com/epita/harrypotterapi/exposition/controllers/RoomsController.java
@@ -1,17 +1,23 @@
 package com.epita.harrypotterapi.exposition.controllers;
 
+import com.epita.harrypotterapi.domain.service.IRoomService;
+import com.epita.harrypotterapi.exposition.request.RoomRequest;
 import com.epita.harrypotterapi.exposition.response.RoomResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
 public class RoomsController {
+    private final IRoomService roomService;
+    
+    public RoomsController(IRoomService roomService) {
+        this.roomService = roomService;
+    }
 
     @GetMapping("/rooms")
     public ResponseEntity<Collection<RoomResponse>> getRooms() {
@@ -21,14 +27,14 @@ public class RoomsController {
     }
 
     @PostMapping("/rooms")
-    public ResponseEntity<RoomResponse> createRoom() {
+    public ResponseEntity<RoomResponse> createRoom(@RequestBody RoomRequest request) {
         var response = new RoomResponse();
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/rooms/csv")
-    public ResponseEntity<Collection<RoomResponse>> createRoomsFromCsv() {
+    public ResponseEntity<Collection<RoomResponse>> createRoomsFromCsv(@RequestParam("file")MultipartFile file) {
         var response = new ArrayList<RoomResponse>();
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
