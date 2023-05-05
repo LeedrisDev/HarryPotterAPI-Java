90af5dd340e88b6f212e4fc31007399f388ebbd6
Thomas Blondel <tfoenix@gmail.com>
2023-05-05 15:45:01 +0200

    [Service] Created interface and class for Room service

---
+++ src/main/java/com/epita/harrypotterapi/infrastructure/service/RoomService.java
@@ -0,0 +1,8 @@
+package com.epita.harrypotterapi.infrastructure.service;
+
+import com.epita.harrypotterapi.domain.service.IRoomService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService implements IRoomService {
+}
