3ced33cfd30c8ddccfe0dffb14fa084287603a75
Thomas Blondel <tfoenix@gmail.com>
2023-05-20 17:56:37 +0200

    [Infrastructure] Tagged MailingService with @Service

--- infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/services/MailingService.java
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/services/MailingService.java
@@ -2,7 +2,9 @@ package com.epita.harrypotterapi.infrastructure.services;
 
 import com.epita.harrypotterapi.domain.models.wizard.Wizard;
 import com.epita.harrypotterapi.domain.services.IMailingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MailingService implements IMailingService {
 
     public void sendMail(Wizard recipient) {
