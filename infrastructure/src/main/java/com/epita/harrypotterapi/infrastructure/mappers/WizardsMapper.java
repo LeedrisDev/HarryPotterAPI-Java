465b6b6969d3e4dc9afca5f0ccb165d5ee8484f7
Thomas Blondel <tfoenix@gmail.com>
2023-05-21 20:01:53 +0200

    [Mapper] Created mapper between domain and entity for wizard

---
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/mappers/WizardsMapper.java
@@ -0,0 +1,27 @@
+package com.epita.harrypotterapi.infrastructure.mappers;
+
+import com.epita.harrypotterapi.domain.models.wizard.Wizard;
+import com.epita.harrypotterapi.infrastructure.entities.WizardEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WizardsMapper {
+    public WizardEntity mapToEntity(Wizard wizard) {
+        var wizardEntity = new WizardEntity();
+        wizardEntity.setName(wizard.getName());
+        wizardEntity.setUsername(wizard.getUsername());
+        wizardEntity.setEmail(wizard.getEmail());
+        wizardEntity.setRole(wizard.getRole());
+
+        return wizardEntity;
+    }
+
+    public Wizard mapToDomain(WizardEntity wizardEntity) {
+        var wizard = new Wizard.Builder()
+                .name(wizardEntity.getName())
+                .username(wizardEntity.getUsername())
+                .role(wizardEntity.getRole());
+
+        return wizard.build();
+    }
+}
