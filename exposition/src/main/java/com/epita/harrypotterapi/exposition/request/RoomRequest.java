2315a43b8f4f43d68fdee123264832a56637754f
Thomas Blondel <tfoenix@gmail.com>
2023-05-21 17:45:48 +0200

    [Request] Created reservation request

--- exposition/src/main/java/com/epita/harrypotterapi/exposition/request/RoomRequest.java
+++ exposition/src/main/java/com/epita/harrypotterapi/exposition/request/RoomRequest.java
@@ -1,13 +1,9 @@
 package com.epita.harrypotterapi.exposition.request;
 
-import java.util.Date;
-
 public class RoomRequest {
     private String name;
     private String type;
     private int area;
-    private Date createdAt;
-    private String createdBy;
 
     public String getName() {
         return name;
@@ -19,13 +15,5 @@ public class RoomRequest {
 
     public int getArea() {
         return area;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
     }
 }
