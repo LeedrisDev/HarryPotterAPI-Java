ff04d081505ac4f6cca47814d07812f88afc51d8
Thomas Blondel <tfoenix@gmail.com>
2023-05-21 20:02:46 +0200

    [JPA] Created magic function to get wizard by username

--- infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/jpa/IWizardRepositoryJPA.java
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/jpa/IWizardRepositoryJPA.java
@@ -4,4 +4,5 @@ import com.epita.harrypotterapi.infrastructure.entities.WizardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IWizardRepositoryJPA extends JpaRepository<WizardEntity, Long> {
+    WizardEntity getWizardEntityByUsername(String username);
 }
