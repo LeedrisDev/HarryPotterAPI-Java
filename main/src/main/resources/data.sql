8df6a091880e95dfaadcf015a1553010592105b4
Thomas Blondel <tfoenix@gmail.com>
2023-05-21 17:40:58 +0200

    [Script] Created to add characters

---
+++ main/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO T_WIZARDS (ID, EMAIL, NAME, ROLE, USERNAME) VALUES
+(1, 'h.potter@hogwarts.com', 'Harry Potter', 'STUDENT', 'h.potter'),
+(2, 'h.granger@hogwarts.com', 'Hermione Granger', 'STUDENT', 'h.granger'),
+(3, 'r.weasley@hogwarts.com', 'Ron Weasley', 'STUDENT', 'r.weasley'),
+(4, 'm.mcgonagall@hogwarts.com', 'Minerva McGonagall', 'PROFESSOR', 'm.mcgonagall'),
+(5, 's.rogue@hogwarts.com', 'Severus Rogue', 'PROFESSOR', 's.rogue'),
+(6, 'a.dumbledore@hogwarts.com', 'Albus Dumbledore', 'DIRECTOR', 'a.dumbledore');
