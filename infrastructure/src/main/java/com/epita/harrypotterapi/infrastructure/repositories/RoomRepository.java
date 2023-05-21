e29144b4dcba1d4a78f946e1af52357f3c024507
Thomas Blondel <tfoenix@gmail.com>
2023-05-21 17:44:03 +0200

    [Repository] Created interface and implementations for rooms

---
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/RoomRepository.java
@@ -0,0 +1,37 @@
+package com.epita.harrypotterapi.infrastructure.repositories;
+
+import com.epita.harrypotterapi.domain.models.room.Room;
+import com.epita.harrypotterapi.domain.repositories.IRoomRepository;
+import com.epita.harrypotterapi.infrastructure.mappers.RoomsMapper;
+import com.epita.harrypotterapi.infrastructure.repositories.jpa.IRoomRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class RoomRepository implements IRoomRepository {
+    private final IRoomRepositoryJPA roomRepository;
+
+    @Autowired
+    public RoomRepository(IRoomRepositoryJPA roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllRooms() {
+        var roomsEntities = this.roomRepository.findAll();
+        return roomsEntities.stream().map(RoomsMapper::mapToDomain).toList();
+    }
+
+    public Room CreateRoom(Room room) {
+        var entity = RoomsMapper.mapToEntity(room);
+        System.out.println(entity);
+        var savedEntity = this.roomRepository.save(entity);
+        return RoomsMapper.mapToDomain(savedEntity);
+    }
+
+    public void CreateRooms(List<Room> rooms) {
+        var entities = rooms.stream().map(RoomsMapper::mapToEntity).toList();
+        this.roomRepository.saveAll(entities);
+    }
+}
