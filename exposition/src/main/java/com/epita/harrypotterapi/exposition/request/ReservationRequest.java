bf1f0bf74f3d57d231164965a6e5800683d3134c
Thomas Blondel <tfoenix@gmail.com>
2023-05-22 02:25:35 +0200

    [Request] Created for reservation

--- exposition/src/main/java/com/epita/harrypotterapi/exposition/request/ReservationRequest.java
+++ exposition/src/main/java/com/epita/harrypotterapi/exposition/request/ReservationRequest.java
@@ -1,4 +1,36 @@
 package com.epita.harrypotterapi.exposition.request;
 
+import java.time.LocalDate;
+
 public class ReservationRequest {
+    String roomName;
+    LocalDate beginDate;
+    LocalDate endDate;
+
+    public ReservationRequest() {
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public LocalDate getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(LocalDate beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
