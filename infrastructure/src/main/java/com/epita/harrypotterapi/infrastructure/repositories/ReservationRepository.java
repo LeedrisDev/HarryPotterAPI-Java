59969ea31ae6a0663c2ab233ca73a684a01af7d8
Thomas Blondel <tfoenix@gmail.com>
2023-05-22 02:22:21 +0200

    [ReservationRepository] Added 2 methods

--- infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/ReservationRepository.java
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/ReservationRepository.java
@@ -1,9 +1,15 @@
 package com.epita.harrypotterapi.infrastructure.repositories;
 
 import com.epita.harrypotterapi.domain.models.Reservation;
+import com.epita.harrypotterapi.domain.models.room.Room;
+import com.epita.harrypotterapi.domain.models.wizard.Wizard;
 import com.epita.harrypotterapi.domain.repositories.IReservationRepository;
+import com.epita.harrypotterapi.infrastructure.mappers.ReservationMapper;
+import com.epita.harrypotterapi.infrastructure.mappers.RoomsMapper;
 import com.epita.harrypotterapi.infrastructure.repositories.jpa.IReservationRepositoryJPA;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,29 +17,44 @@ import java.util.List;
 @Repository
 public class ReservationRepository implements IReservationRepository {
     private final IReservationRepositoryJPA reservationRepositoryJPA;
+    private final RoomsMapper roomsMapper;
+    private final ReservationMapper reservationMapper;
 
     @Autowired
-    public ReservationRepository(IReservationRepositoryJPA reservationRepositoryJPA) {
+    public ReservationRepository(IReservationRepositoryJPA reservationRepositoryJPA,
+                                 @Qualifier("Infrastructure.RoomsMapper") RoomsMapper roomsMapper,
+                                 @Qualifier("Infrastructure.ReservationMapper") ReservationMapper reservationMapper) {
         this.reservationRepositoryJPA = reservationRepositoryJPA;
+        this.roomsMapper = roomsMapper;
+        this.reservationMapper = reservationMapper;
     }
 
-    public Reservation CreateReservation(Reservation reservation) {
+    @Transactional
+    public Reservation createReservation(Reservation reservation) {
+        var reservationEntity = reservationMapper.mapToEntity(reservation);
+        System.out.println(reservationEntity.getWizard());
+        System.out.println(reservationEntity.getRoom());
+        var reservationSaved = reservationRepositoryJPA.save(reservationEntity);
+        return reservationMapper.mapToDomain(reservationSaved);
+    }
+
+    public Reservation getReservationById(Long id) {
         return null;
     }
 
-    public Reservation GetReservationById(Long id) {
+    public List<Reservation> getReservationsByWizard(Wizard wizard) {
         return null;
     }
 
-    public List<Reservation> GetReservationsByWizardId(Long wizardId) {
-        return null;
+    public List<Reservation> getReservationsByRoom(Room room) {
+        var roomEntity = roomsMapper.mapToEntity(room);
+
+        var reservationsEntities = reservationRepositoryJPA.getReservationEntitiesByRoom(roomEntity);
+
+        return reservationsEntities.stream().map(reservationMapper::mapToDomain).toList();
     }
 
-    public List<Reservation> GetReservationsByRoomId(Long roomId) {
-        return null;
-    }
-
-    public void DeleteReservationById(Long id) {
+    public void deleteReservationById(Long id) {
 
     }
 }
