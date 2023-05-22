e7aa80690d94040a227e7d08017e8b70ad4f2f30
Thomas Blondel <tfoenix@gmail.com>
2023-05-22 03:05:22 +0200

    [JPA] Added magic method

--- infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/jpa/IReservationRepositoryJPA.java
+++ infrastructure/src/main/java/com/epita/harrypotterapi/infrastructure/repositories/jpa/IReservationRepositoryJPA.java
@@ -2,10 +2,12 @@ package com.epita.harrypotterapi.infrastructure.repositories.jpa;
 
 import com.epita.harrypotterapi.infrastructure.entities.ReservationEntity;
 import com.epita.harrypotterapi.infrastructure.entities.RoomEntity;
+import com.epita.harrypotterapi.infrastructure.entities.WizardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface IReservationRepositoryJPA extends JpaRepository<ReservationEntity, Long> {
     List<ReservationEntity> getReservationEntitiesByRoom(RoomEntity room);
+    List<ReservationEntity> getReservationEntitiesByWizardId(long wizardId);
 }
